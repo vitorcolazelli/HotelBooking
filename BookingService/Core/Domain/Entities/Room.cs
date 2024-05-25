deda620f8860d4a7788004c2d1f14b1ffb15135e
vitorcolazelli <vitorcolazelli@gmail.com>
2024-05-25 01:34:03 -0300

    Criando objetos de dominio

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+        public bool IsAvailable
+        {
+            get 
+            {
+                if (!InMaintenance || HasGuest)
+                    return false;
+
+                return true;
+            }
+        }
+        public bool HasGuest 
+        {
+            get { return true; }
+        }
+    }
+}
