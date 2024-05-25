55a66ea2030a5dc9649c20af78c778e8007a5d5a
vitorcolazelli <vitorcolazelli@gmail.com>
2024-05-25 16:36:12 -0300

    Criação das tabelas do projeto

--- BookingService/Consumers/API/Program.cs
+++ BookingService/Consumers/API/Program.cs
@@ -1,8 +1,16 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+var connectionString = builder.Configuration.GetConnectionString("Main");
+builder.Services.AddDbContext<HotelDbContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
